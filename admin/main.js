64aac95d9294115d374509fa2c02263c21164663
ktv3mien <kythuatvien3mien@gmail.com>
2026-08-23 23:44:43 +0700

    Update main.js

--- admin/main.js
+++ admin/main.js
@@ -1,11 +1,6 @@
 import { initAuth } from './auth.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Kích hoạt module Authentication (Tự động kéo theo logic Dashboard khi user đã đăng nhập)
-    initAuth();
-});import { initAuth } from './auth.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Kích hoạt module Authentication (Tự động kéo theo logic Dashboard khi user đã đăng nhập)
+    // Kích hoạt module Authentication
     initAuth();
 });
