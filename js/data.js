fd010a22522575e168b6048252ec84e264ee9791
ktv3mien <kythuatvien3mien@gmail.com>
2026-08-23 15:50:09 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,13 +1,13 @@
 // File trung tâm (Hub) để gộp dữ liệu từ các module nhỏ
-import { sequenceData } from './dataSequence.js';
-import { parameterData } from './dataParameter.js';
-import { physicsData } from './dataPhysics.js';
-import { protocolData } from './dataProtocol.js'; // THÊM DÒNG NÀY
+import { sequenceData } from './mri/dataSequence.js';
+import { parameterData } from './mri/dataParameter.js';
+import { physicsData } from './mri/dataPhysics.js';
+import { protocolData } from './mri/dataProtocol.js';
 
-// Gộp cả 4 mảng thành 1 mảng mriData duy nhất
+// Gộp cả 4 mảng thành 1 mảng mriData duy nhất để xuất ra cho main.js và search.js sử dụng
 export const mriData = [
     ...sequenceData, 
     ...parameterData, 
     ...physicsData,
-    ...protocolData // THÊM DÒNG NÀY
+    ...protocolData
 ];
